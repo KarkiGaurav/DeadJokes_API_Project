aaf73e0e8cd1f0eec994461e093267476cf055e9
Gaurav <karkigauravsingh@gmail.com>
2022-12-05 11:19:49 +0530

    this is my first commit

---
+++ jokes.js
@@ -0,0 +1,57 @@
+                                               //   with the help of fetch API     //
+
+
+
+        // const jokes = document.getElementById("joke");
+        // const jokeBtn = document.getElementById("joke-btn");
+        
+        //  const generatejokes = () =>{
+
+        //     const setHeader = {
+        //         headers: {
+        //             Accept : "application/json"
+        //         }
+        //     }
+            
+        //      fetch("https://icanhazdadjoke.com/", setHeader)
+        //      .then((res) => res.json() )
+        //       .then((data) => {
+        //           jokes.innerHTML = data.joke;
+        //       }).catch((error) => {
+        //          console.log(error);
+        //         }) 
+        //     }
+
+
+        // jokeBtn.addEventListener("click", generatejokes);
+        // generatejokes();
+
+
+
+   
+                                                 // with the help of "ASYNC awit"  //
+
+
+        const jokes = document.getElementById("joke");
+        const jokeBtn = document.getElementById("joke-btn");
+        
+         const generatejokes = async() =>{
+        
+            try{
+            const setHeader = {
+                headers: {
+                    Accept : "application/json"
+                }
+            }
+            
+             const res = await fetch("https://icanhazdadjoke.com/", setHeader)
+             const data = await res.json();
+             jokes.innerHTML = data.joke;
+             }catch(error){
+                 console.log(`the error is ${error}`);
+                }
+            }
+
+
+        jokeBtn.addEventListener("click", generatejokes);
+        generatejokes();

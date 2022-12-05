bc9ba1d28b41b0385a4b84d2c48a53cfdad50b7d
Gaurav Singh karki <94061500+KarkiGaurav@users.noreply.github.com>
2022-12-05 11:30:32 +0530

    Update jokes.js
    change the code format.

--- jokes.js
+++ jokes.js
@@ -1,4 +1,35 @@
-                                               //   with the help of fetch API     //
+                                                            // with the help of "ASYNC awit"  //
+
+
+        const jokes = document.getElementById("joke");
+        const jokeBtn = document.getElementById("joke-btn");
+        
+         const generatejokes = async() =>{
+        
+            try{
+            const setHeader = {
+                headers: {
+                    Accept : "application/json"
+                }
+            }
+            
+             const res = await fetch("https://icanhazdadjoke.com/", setHeader)
+             const data = await res.json();
+             jokes.innerHTML = data.joke;
+             }catch(error){
+                 console.log(`the error is ${error}`);
+                }
+            }
+
+
+        jokeBtn.addEventListener("click", generatejokes);
+        generatejokes();
+
+
+
+
+
+                                              //   with the help of fetch API     //
 
 
 
@@ -29,29 +60,3 @@
 
 
    
-                                                 // with the help of "ASYNC awit"  //
-
-
-        const jokes = document.getElementById("joke");
-        const jokeBtn = document.getElementById("joke-btn");
-        
-         const generatejokes = async() =>{
-        
-            try{
-            const setHeader = {
-                headers: {
-                    Accept : "application/json"
-                }
-            }
-            
-             const res = await fetch("https://icanhazdadjoke.com/", setHeader)
-             const data = await res.json();
-             jokes.innerHTML = data.joke;
-             }catch(error){
-                 console.log(`the error is ${error}`);
-                }
-            }
-
-
-        jokeBtn.addEventListener("click", generatejokes);
-        generatejokes();
